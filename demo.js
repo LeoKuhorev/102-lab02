3897ed3a8e199bcddc8ae848b1f9848831d058bf
Leo K <kuhorev.leonid@gmail.com>
2019-05-22 16:00:34 -0700

    added yes/no function

--- demo.js
+++ demo.js
@@ -4,7 +4,8 @@
 var today = new Date();
 var hourNow = today.getHours();
 var greeting;
-var names = [0];
+var names = [];
+var answer;
 //console.log(hourNow);
 
 //changing greeting depending on current time
@@ -18,22 +19,32 @@ if (hourNow > 18) {
   greeting = 'Welcome';
 }
 
+//allow only yes/no answer
+var userInput = function (question) {
+  answer = prompt (question);
+  while (!(answer.toLowerCase() === 'yes' || answer.toLowerCase() === 'no')) {
+    answer = prompt ('I\'m sorry, ' + userName + ', I didn\'t quite get your answer. Please try again using only "yes" and "no" answers');
+  }
+  return answer.toLowerCase();
+};
+
+//draw picture
 var drawKitten = function() {
   document.write ('<img src="https://placekitten.com/100/100"></img>');
 };
 //prompt questions
 var userName = prompt(greeting + ', what\'s your name?');
 //console.log(userName);
-var yesNo = prompt('Nice to meet you, ' + userName + '! Do you want to see a cat picture?');
+var yesNo = userInput('Nice to meet you, ' + userName + '! Do you want to see a cat picture?', 'Yes');
 
 // if user answer "yes" - ask how many, if "no" - bring to the page
-if (yesNo === 'Yes' || yesNo === 'yes' || yesNo === 'YES') {
+if (yesNo === 'yes') {
   var howMany = prompt('Awesome, ' + userName + ', how many cats do you want?');
   var howManyInteger = parseInt (howMany);
   //    console.log(howManyInteger);
   // ask if user wants to give names
-  var namesYesNo = prompt('Do you want to give names to all your ' + howManyInteger + ' cat(s)?');
-  if (namesYesNo === 'Yes' || namesYesNo === 'yes' || namesYesNo === 'YES') { 
+  var namesYesNo = userInput('Do you want to give names to all your ' + howManyInteger + ' cat(s)?', 'Yes');
+  if (namesYesNo === 'yes') { 
     document.write ('<h4> Here are your ' + howManyInteger + ' kitten(s), ' + userName + '</h4>');
     for (var i = 1; i < howManyInteger + 1; i++) {
       names.push(prompt('What\'s the name of your ' + i + ' cat?'));
@@ -42,7 +53,7 @@ if (yesNo === 'Yes' || yesNo === 'yes' || yesNo === 'YES') {
       document.write ('<p>' + names[i] + '</p>');
     //  console.log(names[i]);
     }
-  } else if  (namesYesNo === 'No' || namesYesNo === 'no' || namesYesNo === 'NO') {
+  } else if  (namesYesNo === 'no') {
     document.write ('<h4> Here are your ' + howManyInteger + ' kitten(s), ' + userName + '</h4>');
     for (i = 1; i < howManyInteger + 1; i++) {
       drawKitten();
@@ -53,8 +64,8 @@ if (yesNo === 'Yes' || yesNo === 'yes' || yesNo === 'YES') {
       drawKitten();
     } 
   }
-} else if (yesNo === 'No' || yesNo === 'no' || yesNo === 'NO') {
+} else if (yesNo === 'no') {
   document.write ('<h4>Oh, this is sad, ' + userName + ', but there are some kittens below anyways :-) </h4>');        
-} else {
-  document.write ('<h4>I\'m sorry, ' + userName + ', I didn\'t quite get your answer. Please refresh the page and try one more time using only "yes" and "no" answers</h4>');
-}
+// } else {
+//   document.write ('<h4>I\'m sorry, ' + userName + ', I didn\'t quite get your answer. Please refresh the page and try one more time using only "yes" and "no" answers</h4>');
+// }
